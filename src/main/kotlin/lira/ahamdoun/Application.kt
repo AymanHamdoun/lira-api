81a837e70ca4ce8a2c85269ad807d9c62f37401b
Ayman Hamdoun <ayman.s.hamdoun@gmail.com>
2022-04-15 02:21:25 +0400

    call the lira job every 5 minutes

--- src/main/kotlin/lira/ahamdoun/Application.kt
+++ src/main/kotlin/lira/ahamdoun/Application.kt
@@ -5,10 +5,20 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import lira.ahamdoun.controller.LiraRatesController
-import lira.ahamdoun.plugins.*
+import lira.ahamdoun.jobs.LiraRateJob
 
 fun main() {
+    GlobalScope.launch {
+        while(true) {
+            println("Job Triggerred")
+            LiraRateJob.getAndSaveJobData()
+            Thread.sleep(5 * 60 * 1000)
+        }
+    }
+
     embeddedServer(Netty, port = 8080) {
         routing {
             get("/rates") {
