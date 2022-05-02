8ce89d9ff488ab1fa9816266e7e641d4012a8ce9
Ayman Hamdoun <ayman.s.hamdoun@gmail.com>
2022-05-02 10:52:21 +0300

    add getAll

--- src/main/kotlin/lira/ahamdoun/repositories/BaseRepository.kt
+++ src/main/kotlin/lira/ahamdoun/repositories/BaseRepository.kt
@@ -2,6 +2,7 @@ package lira.ahamdoun.repositories
 
 import lira.ahamdoun.models.BaseModel
 import lira.ahamdoun.utility.Database
+import java.sql.PreparedStatement
 import java.sql.ResultSet
 
 abstract class BaseRepository {
@@ -32,5 +33,31 @@ abstract class BaseRepository {
         })
 
         return model
+    }
+    fun getAll(): MutableList<BaseModel> {
+        return getAllByPreparedStatement(getBaseSelectQuery()) { statement -> statement }
+    }
+
+    fun getAll(conditions: Map<String, Any>): MutableList<BaseModel> {
+        val whereCondition = Database.getWhereConditionStringFromMap(conditions)
+        val sql = getBaseSelectQuery() + " " + whereCondition
+        return getAllByPreparedStatement(sql) { statement ->
+            Database.fillPreparedStatementWithMapConditions(statement, conditions)
+            statement
+        }
+    }
+
+    private fun getAllByPreparedStatement(sql: String, statementHandler: (statement: PreparedStatement) -> PreparedStatement): MutableList<BaseModel> {
+        val models: MutableList<BaseModel> = mutableListOf()
+
+        Database.select(sql, { statement ->
+            statementHandler(statement)
+            statement
+        }, { resultSet ->
+            val model = getObjectFromResultSet(resultSet)
+            models.add(model)
+        })
+
+        return models
     }
 }
